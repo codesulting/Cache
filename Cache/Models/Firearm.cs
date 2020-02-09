4dbaedf2ac2c4450af9856ac1452ab1c5556ca6e
Michael Scribellito <mscribellito@gmail.com>
2020-02-09 11:44:08 -0500

    Update pages

--- Cache/Models/Firearm.cs
+++ Cache/Models/Firearm.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Cache.Models
 {
@@ -7,23 +8,30 @@ namespace Cache.Models
     {
         public int ID { get; set; }
 
+        [Display(Name = "Manufacturer/Importer")]
         public string ManufacturerImporter { get; set; }
 
         public string Model { get; set; }
 
+        [Display(Name = "Serial Number")]
         public string SerialNumber { get; set; }
 
         public string Type { get; set; }
 
+        [Display(Name = "Caliber/Gauge")]
         public string CaliberGauge { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Date Acquired")]
         public DateTime DateAcquired { get; set; }
 
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Cost { get; set; }
 
+        [Display(Name = "Purchase Location")]
         public string PurchaseLocation { get; set; }
 
+        [Display(Name = "Sold/Transferred To")]
         public string SoldTransferredTo { get; set; }
     }
 }
